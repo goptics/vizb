51e77ad678d9fe211baf98437f27dc79331a3fc1
fahimfaisaal <fahimfaisal.soikot@gmail.com>
2025-11-27 12:18:55 +0600

    feat: added source button

--- pkg/template/ui/src/components/IconButton.vue
+++ pkg/template/ui/src/components/IconButton.vue
@@ -1,8 +1,18 @@
+<script setup lang="ts">
+defineProps<{
+  href?: string;
+}>();
+</script>
+
 <template>
-  <button
-    class="inline-flex items-center justify-center w-12 h-12 rounded-lg border border-border bg-card text-card-foreground hover:bg-accent hover:text-accent-foreground shadow-sm"
-    type="button"
+  <component
+    :is="href ? 'a' : 'button'"
+    :href="href"
+    :target="href ? '_blank' : undefined"
+    :rel="href ? 'noopener noreferrer' : undefined"
+    :type="href ? undefined : 'button'"
+    class="inline-flex items-center justify-center w-12 h-12 rounded-lg border border-border bg-card text-card-foreground hover:bg-accent hover:text-primary shadow-sm"
   >
     <slot />
-  </button>
+  </component>
 </template>
